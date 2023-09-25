de24fc91ae5ea4c1b34534f91b6e1eca391cdfee
RAKIB <rakibmahmudmridha@gmail.com>
2023-09-25 21:11:41 +0600

    files added

--- handlers/routeHandlers/sampleHandlers.js
+++ handlers/routeHandlers/sampleHandlers.js
@@ -10,6 +10,7 @@ const handler = {};
 
 handler.sampleHandler = (requestProperties, callback) => {
   console.log(requestProperties);
+
   callback(200, {
     message: "This is a sample url",
   });
--- index.js
+++ index.js
@@ -9,9 +9,25 @@
 const http = require("http");
 const environment = require("./helpers/environments");
 const { handleReqRes } = require("./helpers/handleReqRes");
+const data = require("./lib/data");
 
 //---app object - module scaffolding------
 const app = {};
+
+// testing file system....
+// pore muche dibo...
+// data.create(
+//   "test",
+//   "newFile",
+//   { name: "Bangladesh", language: "Bengali" },
+//   (err) => {
+//     console.log("error was", err);
+//   }
+// );
+
+data.read("test", "newFile", (err, result) => {
+  console.log(err, result);
+});
 
 //----Create server----------------
 
